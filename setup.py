4fafb852709608dd714cc6865950ce5555e369ce
Marek Zajdel <marek.zajdel@ardigen.com>
2022-07-18 15:56:41 +0200

    feat: use pre-fetched IMGT germlines and HMMs

--- setup.py
+++ setup.py
@@ -43,20 +43,10 @@ except Exception as e:
 os.chdir("build_pipeline")
 
 try:
-    shutil.rmtree("curated_alignments/")
-    shutil.rmtree("muscle_alignments/")
-    shutil.rmtree("HMMs/")
-    shutil.rmtree("IMGT_sequence_files/")
     os.mkdir(os.path.join(ANARCI_LOC, "dat"))
 except OSError:
     pass
 
-print('Downloading germlines from IMGT and building HMMs...')
-proc = subprocess.Popen(["bash", "RUN_pipeline.sh"], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-o, e = proc.communicate()
-
-print(o.decode())
-print(e.decode())
-
+print('Copying germlines from IMGT and building HMMs to ANARCI directory...')
 shutil.copy( "curated_alignments/germlines.py", ANARCI_LOC )
 shutil.copytree( "HMMs", os.path.join(ANARCI_LOC, "dat/HMMs/") )
