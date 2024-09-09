d9e804b63acf48a204f9ea4ed045b36e0d2667d3
Jess Ginn <jess@alchemab.com>
2024-09-09 09:32:40 +0100

    fix: formatting of licence

--- lib/python/anarci/anarci.py
+++ lib/python/anarci/anarci.py
@@ -1,7 +1,5 @@
 #    ANARCI - Antibody Numbering and Antigen Receptor ClassIfication
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    Copyright (C) 2016 Oxford Protein Informatics Group (OPIG)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
